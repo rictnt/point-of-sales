e2c0b36c789e28fecae1441b09f973374f6e034b
Emon Khan <bdemon00@gmail.com>
2021-06-10 09:40:50 +0600

    sample route setup

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'admin.dashboard');
 
-Route::resources([
+Route::prefix('admin')->resources([
+    // 'invoices' => App\Http\Controllers\Admin\InvoicesController::class,
     'categories' => App\Http\Controllers\Admin\CategoryController::class,
+    // 'products' => App\Http\Controllers\Admin\ProductController::class,
+    // 'customers' => App\Http\Controllers\Admin\CustomerController::class,
 ]);
