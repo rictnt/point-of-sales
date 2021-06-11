1fef20c933d367487e8b908544a73fc4cff76626
Emon Khan <bdemon00@gmail.com>
2021-06-11 21:01:17 +0600

    Separate Admin Routes

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'admin.dashboard');
 
-Route::namespace('App\Http\Controllers\Admin')->prefix('admin')->as('admin.')->group(function(){
-    Route::resources([
-        // 'invoices' => InvoicesController::class,
-        'categories' => CategoryController::class,
-        // 'products' => ProductController::class,
-        'customers' => CustomerController::class,
-        'suppliers' => SupplierController::class,
-        'users' => UserController::class,
-        'permissions' => PermissionController::class,
-    ]); 
-});
+// all public routes here
+
+Route::get('/', [App\Http\Controllers\Admin\DashboardController::class, 'index']);
+
+
+// ALL ADMIN ROUTES ARE SHIFTED TO routes/admin.php
