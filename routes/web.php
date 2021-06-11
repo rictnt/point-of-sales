be7836b218fda0889710a29caff35ca754d70c78
safiull <safiullalam9931@gmail.com>
2021-06-11 22:14:19 +0600

    Create a new main brunch and resolve conflict

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 
-<<<<<<< HEAD
+
 Route::namespace('App\Http\Controllers\Admin')->prefix('admin')->as('admin.')->group(function(){
     Route::resources([
         // 'invoices' => InvoicesController::class,
@@ -16,11 +16,10 @@ Route::namespace('App\Http\Controllers\Admin')->prefix('admin')->as('admin.')->g
         'expenses' => ExpenseController::class,
     ]); 
 });
-=======
+
 // all public routes here
 
 Route::get('/', [App\Http\Controllers\Admin\DashboardController::class, 'index']);
 
 
 // ALL ADMIN ROUTES ARE SHIFTED TO routes/admin.php
->>>>>>> 0ddce5266f4f2c1575b0146be18c87ca7d0a1369
