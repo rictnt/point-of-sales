c84ff0d384daa2ae5a08ac9dffe02bead36446bd
safiull <safiullalam9931@gmail.com>
2021-06-11 23:12:20 +0600

    resolve

--- routes/web.php
+++ routes/web.php
@@ -4,18 +4,18 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::namespace('App\Http\Controllers\Admin')->prefix('admin')->as('admin.')->group(function(){
-    Route::resources([
-        // 'invoices' => InvoicesController::class,
-        'categories' => CategoryController::class,
-        // 'products' => ProductController::class,
-        'customers' => CustomerController::class,
-        'suppliers' => SupplierController::class,
-        'users' => UserController::class,
-        'expense_categories' => ExpenseCategoryController::class,
-        'expenses' => ExpenseController::class,
-    ]); 
-});
+// Route::namespace('App\Http\Controllers\Admin')->prefix('admin')->as('admin.')->group(function(){
+    // Route::resources([
+    //     // 'invoices' => InvoicesController::class,
+    //     'categories' => CategoryController::class,
+    //     // 'products' => ProductController::class,
+    //     'customers' => CustomerController::class,
+    //     'suppliers' => SupplierController::class,
+    //     'users' => UserController::class,
+    //     'expense_categories' => ExpenseCategoryController::class,
+    //     'expenses' => ExpenseController::class,
+    // ]); 
+// });
 
 // all public routes here
 
