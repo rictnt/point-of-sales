bb06f12b2ee19385218e0aee08961db4c3bc1fa2
Sobuj Hasan <sobujhasan388@gmail.com>
2021-06-13 23:46:58 +0600

    Brand-unit-add

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -1,9 +1,9 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
-
-use App\Models\Product;
 use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Models\Category;
 
 class ProductController extends Controller
 {
@@ -24,7 +24,8 @@ class ProductController extends Controller
      */
     public function create()
     {
-        //
+        $categories = Category::all();
+        return view('admin.products.create', compact('categories'));
     }
 
     /**
@@ -35,7 +36,29 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'product_name' => 'required|max:100',
+            'serial_number' => 'required|integer',
+            'product_model' => 'required|max:200',
+            'category_id' => 'required',
+            'purchase_price' => 'required|integer',
+            'product_unit' => 'required|max:200',
+            'selling_price' => 'required|integer',
+            'tax_vat' => 'required|',
+            'product_details' => 'required|max:5000',
+            'product_image' => 'required | mimes:jpg,jpeg,png,bmp,gif,svg,webp|max:10000',
+        ]);
+
+        // randomly name generate
+        $image_name = Str::random(10) . time() . "." . $request->product_image->getClientOriginalExtension();
+        // catch the photo file
+        $product_photo = $request->file('product_image');
+        // Uplod the photo
+        Image::make($product_photo)->resize(283, 293)->save(base_path('public/image_uploads/product_image/').$image_name, 50);
+
+        Product::create($request->all());
+        notify()->info('Added Successfully', 'Success');
+        return back();
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -8,10 +8,15 @@ Route::namespace('App\Http\Controllers\Admin')->prefix('admin')->as('admin.')->g
     Route::resources([
         // 'invoices' => InvoicesController::class,
         'categories' => CategoryController::class,
-        // 'products' => ProductController::class,
+        'brands' => BrandController::class,
+        'units' => UnitController::class,
+        'products' => ProductController::class,
         'customers' => CustomerController::class,
         'suppliers' => SupplierController::class,
         'users' => UserController::class,
         'permissions' => PermissionController::class,
     ]); 
 });
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
