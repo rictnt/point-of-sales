44c9059d5eaef8be9b37cfe58cabec5aa0658437
Emon Khan <bdemon00@gmail.com>
2021-06-15 17:35:14 +0600

    Expense category updated

--- resources/views/admin/expense_categories/index.blade.php
+++ resources/views/admin/expense_categories/index.blade.php
@@ -1,34 +1,16 @@
 @extends('layouts.app')
 @section('main')
     <div class="page-content">
-        <div class="row">
-            <div class="col-md-12 grid-margin stretch-card">
-                <div class="card">
-                    <div class="card-body">
-                        <h6 class="card-title">Add Expense Category</h6>
-                        <form action="{{ route('admin.expense_categories.store') }}" method="POST" class="forms-sample">
-                            @csrf
-                            <div class="form-group row">
-                                <label class="col-md-2 col-form-label">Expense Category Name</label>
-                                <div class="col-md-4">
-                                    <input type="text" name="name" class="form-control" placeholder="Ex: Shop Rent">
-                                </div>
-                                <div class="col-md-2">
-                                    <button type="submit" class="btn btn-primary form-control">Add</button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-
         <!-- Manage Expense Category` -->
         <div class="row">
             <div class="col-md-12 grid-margin stretch-card">
                 <div class="card">
                     <div class="card-body">
-                        <h6 class="card-title">Manage Expense Category</h6>
+                        <div class="d-flex justify-content-between mb-3">
+                            <h6 class="card-title">Expense Category List</h6>
+                            <button class="btn btn btn-success" data-toggle="modal" data-target="#addModal"><i
+                                    data-feather='plus'></i> Add New</button>
+                        </div>
                         <div class="table-responsive cell-border">
                             <table id="dataTableExample" class="table cell-border">
                                 <thead>
@@ -49,25 +31,7 @@
                                             <td class="{{ $expense_category->status ? 'text-success' : 'text-danger' }}">
                                                 {{ $expense_category->status ? 'Active' : 'Inactive' }}</td>
                                             <td>
-                                                <div class="rapid_action">
-                                                    <button
-                                                        onclick="updateItem({{ $expense_category->id }},null,'toogle')"
-                                                        class="btn {{ $expense_category->status ? 'btn-outline-danger' : 'btn-outline-success' }}">
-                                                        <i data-feather="{{ $expense_category->status ? 'eye-off' : 'eye' }}"
-                                                            style="height: 15px;width: 15px;"></i>
-                                                    </button>
-                                                    <button
-                                                        onclick="updateItem({{ $expense_category->id }},'{{ $expense_category->name }}')"
-                                                        class="btn btn-outline-primary" data-toggle="modal"
-                                                        data-target="#editModal"><i data-feather="edit"
-                                                            style="height: 15px;width: 15px;"></i>
-                                                    </button>
-                                                    <button
-                                                        onclick="confirm('Are you sure want to delete this item?') ? deleteItem({{ $expense_category->id }}):false"
-                                                        class="btn btn-outline-danger"><i data-feather="trash"
-                                                            style="height: 15px;width: 15px;"></i>
-                                                    </button>
-                                                </div>
+                                                @include('admin.components.table-buttons', ['item' => $expense_category])
                                             </td>
                                         </tr>
                                     @endforeach
@@ -79,64 +43,10 @@
             </div>
         </div>
     </div>
-
-    {{-- Edit modal --}}
-    <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editModal" aria-hidden="true">
-        <div class="modal-dialog modal-md">
-            <div class="modal-content">
-                <div class="card">
-                    <div class="card-header">
-                        <h6>Edit Expense Category</h6>
-                    </div>
-                    <div class="card-body">
-                        <form id="updateForm" method="POST">
-                            @csrf
-                            @method('put')
-                            <div class="form-group">
-                                <label>Name</label>
-                                <input type="text" name="name" class="form-control" required>
-                            </div>
-                            <input type="hidden" name="status">
-                            <div class="form-group text-right">
-                                <button class="btn btn-primary">Update</button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    @include('admin.components.create-modal')
+    @include('admin.components.edit-modal')
+    @include('admin.components.delete-modal')
 @endsection
-
-
-@include('admin.components.delete-modal')
-
-
 @push('page-js')
-    <script>
-        function makeUrl(id) {
-            return `{{ route('admin.expense_categories.index') }}/${id}`;
-        }
-
-        function deleteItem(id) {
-            $('#deleteForm').attr('action', makeUrl(id)).submit();
-        }
-
-        function updateItem(id, name, status) {
-
-            // set action on the form 
-            $('#updateForm').attr('action', makeUrl(id));
-
-            // set forms input values 
-            if (name) {
-                $('#updateForm input[name=name]').val(name);
-            }
-
-            if (status == 'toogle') {
-                $('#updateForm input[name=status]').val(status);
-                $('#updateForm').submit();
-            }
-        }
-
-    </script>
+    @include('admin.components.crud-js')
 @endpush
