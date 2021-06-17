f098746e6a5843270bea5ac9ff95f34d57ce3a1c
Emon Khan <bdemon00@gmail.com>
2021-06-18 00:52:12 +0600

    Artisan comand added

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -7,6 +7,7 @@ use App\Models\Brand;
 use App\Models\Product;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ProductController extends Controller
 {
@@ -42,7 +43,7 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        $this->request->validate([
+        $request->validate([
             'category_id' => 'required|numeric',
             'brand_id' => 'required|numeric',
             'unit_id' => 'required|numeric',
@@ -59,19 +60,15 @@ class ProductController extends Controller
             'image' => 'nullable|file|image|mimes:jpg,jpeg,png,bmp,gif,svg,webp|max:1024|dimensions:max_width=5000,max_height=5000',
         ]);
 
-        // print_r($this->request->all());
 
-        $product =  Product::create($this->request->except('image'));
-
-        if (!$product) {
-            return response("Error", 404);
-        }
+        $product =  new Product($request->except('image'));
 
         $product->barcode = $this->createBarCode($product);
 
-        if ($this->request->file('image')) {
-            $product->image = $this->request->image->store('images/products');
+        if ($request->file('image')) {
+            $product->image = $request->image->store('images/products');
         }
+
         $product->save();
 
         notify()->success('Product has been added', 'Success');
@@ -110,61 +107,44 @@ class ProductController extends Controller
      * @param  \App\Models\Product  $product
      * @return \Illuminate\Http\Response
      */
-    public function update(Product $product)
+    public function update(Request $request, Product $product)
     {
-        // $this->request->validate([
-        //     'category_id' => 'required|numeric',
-        //     'brand_id' => 'required|numeric',
-        //     'unit_id' => 'required|numeric',
+        
+        $request->validate([
+            'category_id' => 'required|numeric',
+            'brand_id' => 'required|numeric',
+            'unit_id' => 'required|numeric',
 
-        //     'name' => 'required|string|max:100|min:2|unique:products,name,' .$product->id,
-        //     'alert_stock_quantity' => 'nullable|numeric',
-        //     'barcode_id' => 'nullable|numeric',
-        //     'weight' => 'nullable|string',
-        //     'sku' => 'nullable|string',
-        //     'cost_price' => 'required|not_in:0',
-        //     'sell_price' => 'required|not_in:0',
-        //     'tax' => 'integer',
-        //     'details' => 'required|max:500',
-        //     'image' => 'nullable|file|image|mimes:jpg,jpeg,png,bmp,gif,svg,webp|max:1024|dimensions:max_width=5000,max_height=5000',
-        // ]);
-
-        // if (!$product) {
-        //     return response("Error", 404);
-        // }
-
-        $post =  $product->update([
-            'category_id' => $this->request->category_id,
-            'brand_id' => $this->request->brand_id,
-            'unit_id' => $this->request->unit_id,
-
-            'name' => $this->request->name,
-            'alert_stock_quantity' => $this->request->alert_stock_quantity,
-            'barcode_id' => $this->request->barcode_id,
-            'weight' => $this->request->weight,
-            'sku' => $this->request->sku,
-            'cost_price' => $this->request->cost_price,
-            'sell_price' => $this->request->sell_price,
-            'tax' => $this->request->tax,
-            'details' => $this->request->details,
+            'name' => 'required|string|max:100|min:2',
+            'alert_stock_quantity' => 'nullable|numeric',
+            'barcode_id' => 'nullable|numeric',
+            'weight' => 'nullable|string',
+            'sku' => 'nullable|string',
+            'cost_price' => 'required|not_in:0',
+            'sell_price' => 'required|not_in:0',
+            'tax' => 'integer',
+            'details' => 'required|max:500',
+            'image' => 'nullable|file|image|mimes:jpg,jpeg,png,bmp,gif,svg,webp|max:1024|dimensions:max_width=5000,max_height=5000',
         ]);
 
-        print_r($this->request->name);
 
-        // $product->barcode = $this->createBarCode($product);
+        $product->update($request->except('image'));
 
-        // if ($this->request->file('image')) {
-        //     $product->image = $this->request->image->store('images/products');
+        $product->barcode = $this->createBarCode($product);
 
-        //     // if(Storage::exists($product->image)) {
-        //     //     unlink($product->image); //delete from storage
-        //     // }
-        // }
+        if ($request->file('image')) {
+            $product->image = $request->image->store('images/products');
 
-        // $product->save();
+            if(Storage::exists($product->image)) {
 
-        // notify()->success('Product has been added', 'Success');
-        // return redirect(route('admin.products.index'));
+                // unlink($product->image); //delete from storage
+            }
+        }
+
+        $product->save();
+
+        notify()->success('Product has been added', 'Success');
+        return redirect(route('admin.products.index'));
     }
 
     /**
@@ -181,22 +161,23 @@ class ProductController extends Controller
     }
 
 
-    public function createBarCode($product){
-        $barcode_type = $product->barcode_id ? $product->barcode_id : 1;
+    public function createBarCode($product)
+    {
+        $type = $product->barcode_id ?? 1;
         $number = $product->id;
 
-        if ($barcode_type == 4) { // UPC-A
+        if ($type == 4) {
+            // UPC-A
             $code = '200' . str_pad($number, 8, '0');
-        }
-        else{ // EAN-13 , Others
+        } else {
+            // EAN-13 , Others
             $code = '200' . str_pad($number, 9, '0');
         }
-        
+
         $weightflag = true;
         $sum = 0;
-        for ($i = strlen($code) - 1; $i >= 0; $i--)
-        {
-            $sum += (int)$code[$i] * ($weightflag?3:1);
+        for ($i = strlen($code) - 1; $i >= 0; $i--) {
+            $sum += (int)$code[$i] * ($weightflag ? 3 : 1);
             $weightflag = !$weightflag;
         }
         $code .= (10 - ($sum % 10)) % 10;
