14b6c49e5d81d46a50536dd3aee9061a8195a4c3
Emon Khan <bdemon00@gmail.com>
2021-06-19 18:41:22 +0600

    barcode qty increase improved

--- resources/js/pages/Barcode.vue
+++ resources/js/pages/Barcode.vue
@@ -6,38 +6,25 @@
         <label class="col-sm-3 col-form-label">Select Product:</label>
         <div class="col-sm-9">
           <input
-            @keyup="getProduct($event.target.value)"
+            @keyup="getProduct"
+            v-model="search"
             type="text"
             class="form-control"
+            autofocus
+            placeholder="Enter product name or ID"
           />
-          <div v-if="!foundProducts" class="text-danger">
-            No product found..!
+          <div v-if="foundProducts.length">
+            <ul class="list-group">
+              <li
+                class="list-group-item cursor-pointer"
+                v-for="product in foundProducts"
+                :key="product.id"
+                @click="addProduct(product.id)"
+              >
+                {{ product.name }}
+              </li>
+            </ul>
           </div>
-        </div>
-        <div class="col-md-12">
-          <table v-if="foundProducts.length">
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Product Name</th>
-                <th>Action</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="product in foundProducts" :key="product.id">
-                <td>{{ product.id }}</td>
-                <td>{{ product.name }}</td>
-                <td class="text-center">
-                  <button
-                    @click="addProduct(product.id)"
-                    class="btn btn-success"
-                  >
-                    Select
-                  </button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
         </div>
       </div>
     </div>
@@ -58,9 +45,9 @@
             <td>{{ product.barcode }}</td>
             <td>
               <div class="btn-group" role="group" aria-label="Basic example">
-                <button type="button" class="btn btn-secondary">-</button>
-                <input type="text" class="form-control" :value="product.qty">
-                <button type="button" class="btn btn-secondary">+</button>
+                <button @click="decreaseQty(product.id)" type="button" class="btn btn-secondary">-</button>
+                <input type="text" class="form-control" :value="product.qty" />
+                <button @click="increaseQty(product.id)" type="button" class="btn btn-secondary">+</button>
               </div>
             </td>
             <td>
@@ -76,18 +63,19 @@
 </template>
 
 <script>
-
 export default {
   data() {
     return {
+      search: "",
       foundProducts: [],
       addedProducts: [],
     };
   },
   methods: {
-    async getProduct(q) {
-      let route = `/api/products?q=${q}`;
-      if (q) {
+    
+    async getProduct() {
+      let route = `/api/products?q=${this.search}`;
+      if (this.search != '') {
         await axios(route)
           .then((res) => {
             this.foundProducts = res.data ?? "";
@@ -99,24 +87,46 @@ export default {
         this.foundProducts = [];
       }
     },
+
     addProduct(id) {
-      let currentProduct = this.foundProducts.find(
+      let currentProduct = this.foundProducts.find(product => product.id == id)
+      let checkIfExist = this.addedProducts.find(
         (product) => product.id == id
       );
 
-      let checkIfExist = this.addedProducts.find(
-        (product) => product.id == currentProduct.id
-      );
-
       if (!checkIfExist) {
+        currentProduct.qty = 1
         this.addedProducts.push(currentProduct);
       } else {
         toastr.info("Product Already Added");
       }
+      this.search = "";
+      this.foundProducts = [];
     },
+
     removeProduct(id) {
       this.addedProducts.pop((product) => product.id == id);
     },
+
+    decreaseQty(id){
+
+    return this.addedProducts.filter(product => {
+       if (product.id == id && product.qty != 1) {
+           product.qty -= 1
+       }
+       return product
+     })
+    },
+
+    increaseQty(id){
+
+    return this.addedProducts.filter(product => {
+       if (product.id == id) {
+         product.qty += 1
+       }
+       return product
+     })
+    }
   },
 };
 </script>
