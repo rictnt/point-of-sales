54329c0dd29ed9b8a1b56840114feebb13d1f748
Emon Khan <bdemon00@gmail.com>
2021-06-21 21:34:36 +0600

    Topbar updated

--- resources/views/layouts/partials/topbar.blade.php
+++ resources/views/layouts/partials/topbar.blade.php
@@ -22,7 +22,7 @@
                     <div class="dropdown-body">
                         <ul class="profile-nav p-0 pt-3">
                             <li class="nav-item">
-                                <a href="pages/general/profile.html" class="nav-link">
+                                <a href="#" class="nav-link">
                                     <i data-feather="user"></i>
                                     <span>Profile</span>
                                 </a>
